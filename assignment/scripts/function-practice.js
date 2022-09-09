62bd15ec621db56c0ff42c6ff7b4c962194b00ea
Alina Trukhina <alina.trukhina@gmail.com>
2022-09-08 20:53:33 -0500

    finished 8-10

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -91,22 +91,41 @@ console.log('find - should say false', find(4, [1,2,3]));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter === string[0]) {
+    return true;
+  } return false;
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
   // TODO: loop to add items
+  for (let n in array) {
+    sum += array[n];   
+    }
   return sum;
 }
+
+console.log('sumAll - should be 6', sumAll([1,2,3]));
+console.log('sumAll - should be 6', sumAll([1,-2,3,-2]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+function posArray(array) {
+  let positiveNum = [];
+  for (let n in array) {
+    if (array[n] > 0) {
+      positiveNum.push(array[n]);
+    }
+  } return positiveNum;
+}
+
+console.log('posArray - should be [1,2]', posArray([-1,1,-2,2]));
+console.log('posArray - should be empty', posArray([-1,-2,-3,-4]));
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
